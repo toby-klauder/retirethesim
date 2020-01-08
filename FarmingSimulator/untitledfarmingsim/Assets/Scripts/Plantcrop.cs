b22c97e67e64567dc2d0f168793bab14892e8721
Toby Klauder <toby.klauder@digipen.edu>
2020-01-08 09:45:42 -0800

    add basic plant mechanics

---
+++ FarmingSimulator/untitledfarmingsim/Assets/Scripts/Plantcrop.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Plantcrop : MonoBehaviour
+{
+    public GameObject wheat;
+    public bool hascrop; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnMouseDown()
+    {
+        if (!hascrop) {
+            Instantiate(wheat);
+            hascrop = true; 
+        }
+        
+    }
+}
