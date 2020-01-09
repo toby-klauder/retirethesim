dcd5fab327a22fca5f3c750c076038d2e0df2faa
Toby Klauder <toby.klauder@digipen.edu>
2020-01-09 07:49:57 -0800

    script and sprites

---
+++ FarmingSimulator/untitledfarmingsim/Assets/Scripts/Gamemanager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gamemanager : MonoBehaviour
+{
+
+    public static int wheat;
+    public static string hand = "none";
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ FarmingSimulator/untitledfarmingsim/Assets/Scripts/timecontrol.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class timecontrol : MonoBehaviour
+{
+
+    public Sprite stageone;
+    public Sprite stagetwo;
+    public Sprite stagefinal;
+    public double growtime;
+    public static SpriteRenderer swaprender; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        swaprender = GetComponent<SpriteRenderer>(); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (swaprender.sprite == stageone) {
+            growtime += Time.deltaTime; 
+        }
+        //grow
+        if (growtime > 15) {
+            swaprender.sprite = stagetwo; 
+        }
+        if (growtime > 30) {
+            swaprender.sprite = stagefinal; 
+        }
+    }
+}
