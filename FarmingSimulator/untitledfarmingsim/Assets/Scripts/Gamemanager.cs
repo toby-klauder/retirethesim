f1f956b2a3deee29a1af8703c67426eccdfe842d
Toby Klauder <toby.klauder@digipen.edu>
2020-01-09 09:17:57 -0800

    good stuff

--- FarmingSimulator/untitledfarmingsim/Assets/Scripts/Gamemanager.cs
+++ FarmingSimulator/untitledfarmingsim/Assets/Scripts/Gamemanager.cs
@@ -6,16 +6,54 @@ public class Gamemanager : MonoBehaviour
 {
 
     public static int wheat;
+    public static int money;
+    public static int barley;
+    public static int corn;
+    public static int potato;
+    public static int carrot;
+    public static int wheatseed;
+    public static int barleyseed;
+    public static int cornseed;
+    public static int potatoseed;
+    public static int carrotseed; 
     public static string hand = "none";
+
+    private static Gamemanager _instance;
+
+    public static Gamemanager instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = FindObjectOfType<Gamemanager>();
+                DontDestroyOnLoad(_instance);
+            }
+            return _instance;
+        }
+    }
+
+    void Awake()
+    {
+        if (_instance == null)
+        {
+            _instance = this;
+            DontDestroyOnLoad(this);
+        }
+        else if(_instance != this)
+        {
+            Destroy(this);
+        }
+    }
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
