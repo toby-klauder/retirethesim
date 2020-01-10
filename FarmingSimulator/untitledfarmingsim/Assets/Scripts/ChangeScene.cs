5cf3e01c0a6c3adff2aaaf112eb49259956ac4a1
Adeline Braun <adeline.braun@digipen.edu>
2020-01-10 09:41:59 -0800

    Added more to the shop

---
+++ FarmingSimulator/untitledfarmingsim/Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class ChangeScene : MonoBehaviour
+{
+    public string scene = "test";
+    public void changeScene()
+    {
+        SceneManager.LoadScene(scene);
+
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+}
---
+++ FarmingSimulator/untitledfarmingsim/Assets/Scripts/MoneyManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MoneyManager : MonoBehaviour
+{
+    public Text txt;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        txt.text = "$" + Gamemanager.money;
+    }
+
+    public void purchase(string crop, int cost)
+    {
+        if(Gamemanager.money >= cost)
+        {
+            Gamemanager.money -= cost;
+            Gamemanager.
+        }
+      
+    }
+
+    public void sell(string crop, int cost)
+    {
+        Gamemanager.money += cost;
+    }
+}
