8fdc5726b1a0fbea8152959a1d6a7414597d56cc
Adeline Braun <adeline.braun@digipen.edu>
2020-01-17 09:10:35 -0800

    Added stuff to shop

--- FarmingSimulator/untitledfarmingsim/Assets/Scripts/MoneyManager.cs
+++ FarmingSimulator/untitledfarmingsim/Assets/Scripts/MoneyManager.cs
@@ -6,6 +6,17 @@ using UnityEngine.UI;
 public class MoneyManager : MonoBehaviour
 {
     public Text txt;
+    public int wheatBuyCost;
+    public int wheatSellCost;
+    public int barleyBuyCost;
+    public int barleySellCost;
+    public int cornBuyCost;
+    public int cornSellCost;
+    public int potatoBuyCost;
+    public int potatoSellCost;
+    public int carrotBuyCost;
+    public int carrotSellCost;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,21 +26,68 @@ public class MoneyManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        txt.text = "$" + Gamemanager.money;
+       txt.text = "$" + Gamemanager.money;
     }
 
-    public void purchase(string crop, int cost)
+    public void purchase(string crop)
     {
-        if(Gamemanager.money >= cost)
-        {
-            Gamemanager.money -= cost;
-            Gamemanager.
-        }
+            if (crop.Equals("wheat") && Gamemanager.money >= wheatBuyCost)
+            {
+                Gamemanager.seed_wheat += 1;
+                Gamemanager.money -= wheatBuyCost;
+            }
+            else if (crop.Equals("barley") && Gamemanager.money >= barleyBuyCost)
+            {
+                Gamemanager.seed_barley += 1;
+                Gamemanager.money -= barleyBuyCost;
+            }
+            else if (crop.Equals("corn") && Gamemanager.money >= cornBuyCost)
+            {
+                Gamemanager.seed_corn += 1;
+                Gamemanager.money -= cornBuyCost;
+            }
+            else if (crop.Equals("potato") && Gamemanager.money >= potatoBuyCost)
+            {
+                Gamemanager.seed_potato += 1;
+                Gamemanager.money -= potatoBuyCost;
+            }
+            else if (crop.Equals("carrot") && Gamemanager.money >= carrotBuyCost)
+            {
+                Gamemanager.seed_carrot = +1;
+                Gamemanager.money -= carrotBuyCost;
+            }
+        
       
     }
 
-    public void sell(string crop, int cost)
+
+    public void sell(string crop)
     {
-        Gamemanager.money += cost;
+        if (crop.Equals("wheat") && Gamemanager.seed_wheat > 0)
+        {
+            Gamemanager.seed_wheat -= 1;
+            Gamemanager.money += wheatSellCost;
+
+        }
+        else if (crop.Equals("barley") && Gamemanager.seed_barley > 0)
+        {
+            Gamemanager.seed_barley -= 1;
+            Gamemanager.money += barleySellCost;
+        }
+        else if (crop.Equals("corn") && Gamemanager.seed_corn > 0)
+        {
+            Gamemanager.seed_corn -= 1;
+            Gamemanager.money += cornSellCost;
+        }
+        else if (crop.Equals("potato") && Gamemanager.seed_potato > 0)
+        {
+            Gamemanager.seed_potato -= 1;
+            Gamemanager.money += potatoSellCost;
+        }
+        else if (crop.Equals("carrot") && Gamemanager.seed_carrot > 0)
+        {
+            Gamemanager.seed_carrot -= 1;
+            Gamemanager.money += carrotSellCost;
+        }
     }
 }
